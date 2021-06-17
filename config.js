99f2487900dbf170e5d8f6d4cc3e5d650033000d
Q.Tran <tranlehaiquan@gmail.com>
2021-06-17 18:40:06 +0700

    update build fail

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://uyz7pnfy.api.sanity.io/v1/graphql/production/default`;
-export const prodEndpoint = `http://localhost:3000/api/graphql`;
+export const endpoint = process.env.SANITY_GRAPHQL;
+export const prodEndpoint = process.env.SANITY_GRAPHQL;
 export const perPage = 4;
